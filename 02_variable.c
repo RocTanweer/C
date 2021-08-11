3b3cd4be936d71d36108aa6da5ab8454048d5eaf
RocTanweer <mohammadtanweer769@gmail.com>
2021-08-11 14:17:43 +0530

    initial commit

---
+++ 02_variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    // int b = 8.5 // not recommended
+    float b = 8.5;
+    char c = 'a';
+    int d = 45;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of b is '%c' \n", c);
+    printf("The sum of a and d is %d \n", a + d);
+    return 0;
+}
---
+++ 04_arithmetic_instruction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    int b = 8;
+
+    printf("The value of a + b is : %d\n", a + b);
+    printf("The value of a - b is : %d\n", a - b);
+    printf("The value of a * b is : %d\n", a * b);
+    printf("The value of a / b is : %d\n", b / a);
+    return 0;
+}
