98ad79a26d9311a120a8d09b7dee86344a6c9f4c
RocTanweer <mohammadtanweer769@gmail.com>
2022-02-28 09:20:42 +0530

    wrote intro in README | added lib | added mario dir

---
+++ README.md
@@ -0,0 +1,9 @@
+# Computer Fundamentals
+
+**This repository contains code and concepts related to _Introduction to Computer Science_ topic**
+
+## Table of Contents
+
+- [mario: C](/mario)
+  - [easy](/mario/easy/mario_easy.c)
+  - [hard](/mario/hard/mario_hard.c)
---
+++ mario/easy/mario_easy.c
@@ -0,0 +1,27 @@
+#include "../../../C/lib/cs50.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int Height;
+
+    // Keep asking user for the input until it matches the condition
+    do
+    {
+        Height = get_int("Height: ");
+    } while (Height < 1 || Height > 8);
+
+    // Looping through 0 to Height twice, for rows and it's corresponding columns
+    for (int i = 0; i < Height; i++) // for rows
+    {
+        for (int j = 0; j < Height - (i + 1); j++) // for ' ' part of column
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < (i + 1); k++) // for '#' part of column
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
