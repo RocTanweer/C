40d9326c5686a3a8818efbb7df9a771817936bc3
RocTanweer <mohammadtanweer769@gmail.com>
2022-03-07 15:09:02 +0530

    added recursion to full pyramid and removed loop

--- mario/hard/mario_hard.c
+++ mario/hard/mario_hard.c
@@ -1,6 +1,8 @@
 #include "../../lib/cs50.h"
 #include <stdio.h>
 
+void make_pyramid(int num_of_rows, int space_count);
+
 int main(void)
 {
     int Height;
@@ -10,25 +12,39 @@ int main(void)
         Height = get_int("Height: ");
     } while (Height < 1 || Height > 8); // => condition
 
-    // Looping through 0 to Height twice, for rows and it's corresponding columns
-    for (int i = 0; i < Height; i++) // for rows
+    /**
+     * Prints a full volcano mountain
+     * @param {int} Height - number of rows which determines the height of it
+     * @param {const} 0 - Number of spaces in each row, zero since bottom row contains 0 spaces
+     */
+    make_pyramid(Height, 0);
+}
+
+void make_pyramid(int num_of_rows, int space_count)
+{
+    // condition(s) to stop the recursion
+    if (num_of_rows <= 0)
     {
-        for (int j = 0; j < Height - (i + 1); j++) // for ' ' part of column
-        {
-            printf(" ");
-        }
-        for (int k = 0; k < (i + 1); k++) // for '#' part of column
-        {
-            printf("#");
-        }
-
-        printf("  "); // for '  ' part of column
-
-        for (int l = 0; l < (i + 1); l++) // for '#' part of column
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        return;
     }
+
+    // calling the function again with different argument
+    make_pyramid(num_of_rows - 1, space_count + 1);
+
+    // printing current row
+    for (int j = 0; j < space_count; j++)
+    {
+        printf(" ");
+    }
+
+    for (int i = 0; i < num_of_rows; i++)
+    {
+        printf("#");
+    }
+    printf(" ");
+    for (int i = 0; i < num_of_rows; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
