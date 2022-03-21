b76c34f8caef9c7a193f7e65416c12f702f8d4a0
RocTanweer <mohammadtanweer769@gmail.com>
2022-03-21 16:32:12 +0530

    UPDATE: created data structure and assigining initial value at generals/charfeq

--- generals/charfreq/charfreq.c
+++ generals/charfreq/charfreq.c
@@ -1,9 +1,75 @@
 #include <stdio.h>
+#include <string.h>
+
+void assignValue();
+void incrementCount(char c);
+
+typedef struct Chars
+{
+    char name;
+    int count;
+} Char;
+
+// global var
+Char characters[52];
 
 int main(int argc, char *argv[])
 {
-    printf("The number of arguments are: %d\n", argc - 1);
+    assignValue();
 
-    printf("first argument is: %s\n", argv[1]);
+    // for (int l = 0; l < 52; l++)
+    // {
+    //     printf("%c ", characters[l].name);
+    //     printf("%d ", characters[l].count);
+    //     printf("\n");
+    // }
+
+    // for (int j = 1; j < argc; j++)
+    // {
+    //     int len = strlen(argv[j]);
+    //     for (int k = 0; k < len; k++)
+    //     {
+    //         incrementCount(argv[j][k]);
+    //     }
+    // }
+
+    // for (int l = 0; l < 52; l++)
+    // {
+    //     printf("%c ", characters[l].name);
+    //     printf("%d ", characters[l].count);
+    //     printf("\n");
+    // }
+
     return 0;
+}
+
+void assignValue()
+{ // Assigning the values
+    for (int i = 0; i < 52; i++)
+    {
+        if (i < 26)
+        {
+            characters[i].name = (char)('A' + i);
+            characters[i].count = 0;
+        }
+        else
+        {
+            characters[i].name = (char)('A' + i + 6);
+            characters[i].count = 0;
+        }
+        printf("%c ", characters[i].name);
+        printf("%i", characters[i].count);
+        printf("\n");
+    }
+}
+
+void incrementCount(char c)
+{
+    for (int i = 0; i < 52; i++)
+    {
+        if (characters[i].name == c)
+        {
+            characters[i].count++;
+        }
+    }
 }
