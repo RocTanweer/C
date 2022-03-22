fe255b29390096bf171d918a9ac5b82362ef4942
RocTanweer <mohammadtanweer769@gmail.com>
2022-03-22 08:17:00 +0530

    ADD: program to show a table of frequency of alphabets in a text provided as cmdline arg

--- generals/charfreq/charfreq.c
+++ generals/charfreq/charfreq.c
@@ -17,28 +17,21 @@ int main(int argc, char *argv[])
 {
     assignValue();
 
-    // for (int l = 0; l < 52; l++)
-    // {
-    //     printf("%c ", characters[l].name);
-    //     printf("%d ", characters[l].count);
-    //     printf("\n");
-    // }
+    for (int j = 1; j < argc; j++)
+    {
+        int len = strlen(argv[j]);
+        for (int k = 0; k < len; k++)
+        {
+            incrementCount(argv[j][k]);
+        }
+    }
 
-    // for (int j = 1; j < argc; j++)
-    // {
-    //     int len = strlen(argv[j]);
-    //     for (int k = 0; k < len; k++)
-    //     {
-    //         incrementCount(argv[j][k]);
-    //     }
-    // }
-
-    // for (int l = 0; l < 52; l++)
-    // {
-    //     printf("%c ", characters[l].name);
-    //     printf("%d ", characters[l].count);
-    //     printf("\n");
-    // }
+    for (int l = 0; l < 52; l++)
+    {
+        printf("%c ", characters[l].name);
+        printf("%d", characters[l].count);
+        printf("\n");
+    }
 
     return 0;
 }
@@ -49,17 +42,14 @@ void assignValue()
     {
         if (i < 26)
         {
-            characters[i].name = (char)('A' + i);
+            characters[i].name = 'A' + i;
             characters[i].count = 0;
         }
         else
         {
-            characters[i].name = (char)('A' + i + 6);
+            characters[i].name = 'A' + i + 6;
             characters[i].count = 0;
         }
-        printf("%c ", characters[i].name);
-        printf("%i", characters[i].count);
-        printf("\n");
     }
 }
 
